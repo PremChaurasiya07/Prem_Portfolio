5fd330c49c9a6406889820d370a60f4247534d0d
PremChaurasiya07 <premchaurasiya2006@gmail.com>
2025-01-10 12:46:45 +0530

    img added

--- src/utils/repo.js
+++ src/utils/repo.js
@@ -1,7 +1,7 @@
 export const repo=[
     {
         id:"01",
-        img:'src/utils/img1.png',
+        img:'public/img1.png',
         name:"Product Management System",
         des:'This is a product management system in which user can add their product info like name, price, description for future reference.In this, authentication system is also setup using jwt.',
         tech:'express, react, mongoodb, tailwind, jwt, multer, etc.',
@@ -11,7 +11,7 @@ export const repo=[
     },
     {
         id:"02",
-        img:'src/utils/img2.png',
+        img:'public/img1.png',
         name:"Ai Interview Webapp",
         des:'This was a college project done with me teamates.It help the candidate to prepare for the interview by asking the field of interview by user and depending upon that question is asked from gemini api.Mic intergration is there to record user answer, after interview review is also given.',
         tech:'Next.js, dizzleorm, tailwind, Gemini api, etc.',
